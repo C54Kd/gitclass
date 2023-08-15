d029f5626be289730389975cb7af59b07a103689
kk <Ei258CQ@gmail.com>
2023-08-15 14:02:52 +0900

    script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-a
+console.log("hello javascript")
+
+
+// 변수 선언 함수
+
+
+
+var age = 25;
+let year = 2023; // 문자, 숫자
+const point = 250;
+
+
+// data type.
+// Number = 1,2,3,4,5,6
+
+
+
+
+console.log(age);
+console.log(year);
+console.log(point);
